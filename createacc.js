b25d778cf45164574ec97319210e497ee0789513
Navya C <navyc122@in.ibm.com>
2020-09-29 17:56:37 +0530

    login

--- createacc.js
+++ createacc.js
@@ -15,19 +15,19 @@ function checkPass()
         message.innerHTML = "Passwords Do Not Match!"
     }
 } 
-function email_validate(email)
-{
-var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
+// function email_validate(email)
+// {
+// var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
 
-    if(regMail.test(email) == false)
-    {
-    document.getElementById("status").innerHTML = "<span class='text-danger'>Email address is not valid yet.</span>";
-    }
-    else
-    {
-    document.getElementById("status").innerHTML	= "<span class='text-success'>Thanks, you have entered a valid Email address!</span>";	
-    }
-}
+//     if(regMail.test(email) == false)
+//     {
+//     document.getElementById("status").innerHTML = "<span class='text-danger'>Email address is not valid yet.</span>";
+//     }
+//     else
+//     {
+//     document.getElementById("status").innerHTML	= "<span class='text-success'>Thanks, you have entered a valid Email address!</span>";	
+//     }
+// }
 function validate()
 {
     console.log("enter");
