e3e4659d3de57722bc8a1f789e268f60525b019b
Navya C <navyc122@in.ibm.com>
2020-09-30 15:15:10 +0530

    node

--- node-js Practise/app.js
+++ node-js Practise/app.js
@@ -1,17 +1,17 @@
 
 
-const  addition=require('./calculation');
+const  addition=require('../calculation');
 addition.addMethod(200,200);
 console.log(addition);
 
-const  subtrac=require('./calculation');
+const  subtrac=require('../calculation');
 addition.subMethod(200,200);
 console.log(subtrac);
 
-const  multiplication=require('./calculation');
+const  multiplication=require('../calculation');
 addition.multiMethod(200,200);
 console.log(multiplication);
 
-const  division=require('./calculation');
+const  division=require('../calculation');
 addition.divMethod(200,200);
 console.log(division);
