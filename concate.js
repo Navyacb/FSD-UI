48b562e1a5d52c78846056bfe248f71ba6db452c
Navya C <navyc122@in.ibm.com>
2020-09-30 12:40:49 +0530

    concat

--- concate.js
+++ concate.js
@@ -2,4 +2,4 @@ const returnFirstName = require('./name');
 const returnSecondName = require('./name');
 let firstName = returnFirstName.fn('John');
 let secondName = returnSecondName.sn('dan');
-console.log(firstName+" "+secondName);
+console.log("Concatination of first and second name is: "+firstName+" "+secondName);
