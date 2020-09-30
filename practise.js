0d628937025e3c46cab811f1a188d0ca9a8b2ed0
Navya C <navyc122@in.ibm.com>
2020-09-30 15:52:17 +0530

    test

--- practise.js
+++ practise.js
@@ -12,11 +12,29 @@
 
 
     
-const fs = require('fs');
-fs.readdir('...../', (err,data)=> {
-    if (err)
-        console.log('error occurred');
-    else
-        console.log(data);
-});
+// const fs = require('fs');
+// fs.readdir('...../', (err,data)=> {
+//     if (err)
+//         console.log('error occurred');
+//     else
+//         console.log(data);
+// });
+
+// const path = require('path');
+// let x=path.parse(__filename);
+// console.log(x);
+// let y=path.dirname('/foo/bar/baz/asdf/quux');
+// console.log(y);
+
+function myTest(message)
+{
+    console.log(message);
+    setTimeout(myTest1,20000);
+    console.log('hello');
+}
+function myTest1()
+{
+    console.log('Hi');
+}
+myTest('Hi I Am John Doe ');
 
