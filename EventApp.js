25514af3267dd66c86f9d1454be62cf261d0fa12
Navya C <navyc122@in.ibm.com>
2020-10-01 11:16:21 +0530

    evenr

--- EventApp.js
+++ EventApp.js
@@ -1,7 +1,23 @@
+// const EventEmitter = require('events');
+// const emitter=new EventEmitter();
+// const loggerCall=require('./EventLogger');
+// emitter.on('messageLogged',()=>{
+//     loggerCall.logger('hello world');
+// });
+// console.log('event executed successfully');
+
+
+  
 const EventEmitter = require('events');
-const emitter=new EventEmitter();
-const loggerCall=require('./Eventlogger');
-emitter.on('messageLogged',()=>{
-    loggerCall.logger('hello world');
+const Logger=require('./EventLogger');
+const logger=new Logger();
+logger.log('calling log method from logger module');
+logger.log1('calling log1 method from logger module');
+logger.addListener('messageLogged',()=>{
+    console.log('Handled messageLogged Event');
 });
-console.log('event executed successfully');
+logger.addListener('messageLogged',()=>
+{
+   console.log('Handled messageLogged1 Event');
+});
+logger.log('==END==');
