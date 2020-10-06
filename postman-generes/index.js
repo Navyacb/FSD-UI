3a44ec8609c01d7b05e121bb52532d78d38e9037
Navya C <navyc122@in.ibm.com>
2020-10-06 15:27:19 +0530

    put method

--- postman-generes/index.js
+++ postman-generes/index.js
@@ -1,3 +1,4 @@
+const Joi=require('joi');
 const express=require('express');
 const app=express();
 app.use(express.json());
@@ -23,27 +24,56 @@ app.get('/api/generes/:id', (req, res) => {
 
 })
 app.get('/api/generes/:name', (req, res) => {
-    let id=parseInt(req.params.name);
-    const genre = generes.find(c => c.id === id);
-    if (!genre)
-        return res.status(404).send(`The genre with the ID ${id} was not found.`);
+        let name = String(req.params.name);
+        console.log(name);
+        const genre1= generes.find(c => c.name === name);
+    
+        if (!genre1)
+            return res.status(404).send(`The genre with the Name ${name} was not found.`);
+        res.send(genre1);
+    });
 
-      // res.send(`the genere with ID ${id} found${genre}`);
-       res.send(genre);
-
-
-})
-
+// app.post('/api/generes', (req, res) => {
+// const genre={
+//     id:generes.length+1,
+//     name:req.body.name
+// };
+// generes.push(genre);
+// res.send(genre);
+// });
 app.post('/api/generes', (req, res) => {
-const genre={
-    id:generes.length+1,
-    name:req.body.name
-};
-generes.push(genre);
-res.send(genre);
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    const genre = {
+        id: generes.length + 1,
+        name: req.body.name
+    };
+    generes.push(genre);
+    res.send(genre);
 });
+
+app.put('/api/generes/:id', (req, res) => {
+    const genre = generes.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    console.log(genre.name);
+    genre.name = req.body.name;
+    console.log(genre.name);
+    res.send(genre);
+});
+
 let port=process.env.PORT || 3000;
 app.listen(port, function(){
     console.log('server started listening on port number',port);
 })
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(5).required()
+    };
+
+    return Joi.validate(genre, schema);
+}
