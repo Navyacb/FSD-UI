617bd4bd8be2bad9864d4863b0158daeea19879f
Navya C <navyc122@in.ibm.com>
2020-10-06 16:07:22 +0530

    delete

--- Movie-Rating/index.js
+++ Movie-Rating/index.js
@@ -3,9 +3,9 @@ const express=require('express');
 const app=express();
 app.use(express.json());
 let movies=[
-    {id:1,name: 'Sipderman'},
-    {id:2,name: 'Batman'},
-    {id:3,name: 'Little Women'}
+    {id:1,name: 'Sipderman',rating:3},
+    {id:2,name: 'Batman',rating:5},
+    {id:3,name: 'Little Women',rating:4}
 ];
 app.get('/api/movies', (req, res) => {
     res.send(movies);
@@ -29,13 +29,21 @@ app.get('/api/movies1/:name', (req, res) => {
         res.send(movie1);
     });
 
+app.get('/api/movies2/:rating', (req, res) => {
+        let rating=parseInt(req.params.rating);
+        const movie = movies.find(c => c.rating === rating);
+        if (!movie)
+            return res.status(404).send(`The movie with the rating ${rating} was not found.`);
+             res.send(movie);
+    })
 app.post('/api/movies', (req, res) => {
     const { error } = validatemovies(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     const movie = {
         id: movies.length + 1,
-        name: req.body.name
+        name: req.body.name,
+        rating:req.body.rating
     };
     movies.push(movie);
     res.send(movie);
@@ -51,6 +59,17 @@ app.put('/api/movies/:id', (req, res) => {
     movie.name = req.body.name;
     console.log(movie.name);
     res.send(movie);
+});
+app.delete('/api/movies/:id', (req, res) => {
+    const movie = movies.find(c => c.id === parseInt(req.params.id));
+    if (!movie) {
+        return res.status(404).send('The movie with the given ID was not found.');
+    }
+    const index=movies.indexOf(movie);
+    movies.splice(index, 1);
+    res.send(movie);
+
+
 });
 
 let port=process.env.PORT || 3000;
