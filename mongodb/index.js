2932764ea2ef0a466fff7dd8b892e8ca2b31678b
unknown <ibmjavafse06@iiht.tech>
2020-10-08 12:21:06 +0530

    mongo

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/play-ground',{ useNewUrlParser: true , useUnifiedTopology: true} )
+mongoose.connect('mongodb://localhost:27017/local',{ useNewUrlParser: true , useUnifiedTopology: true} )
 .then(function(){
     console.log('Connected to Mongo');
 })
@@ -7,15 +7,15 @@ mongoose.connect('mongodb://localhost:27017/play-ground',{ useNewUrlParser: true
     console.log('Connection is not Unsuccessful..!',reason.message);
 })
 
-// const studentSchema=new mongoose.Schema(
-//     {
-//         firstName:String,
-//         lastName:String,
-//         email:String,
-//         doj:{type: Date,default:Date.now()}
-//     });
+const studentSchema=new mongoose.Schema(
+    {
+        firstName:String,
+        lastName:String,
+        email:String,
+        doj:{type: Date,default:Date.now()}
+    });
 
-// const Student=mongoose.model('Student',studentSchema);
+const Student=mongoose.model('Student',studentSchema);
 // let student=new Student({
 //     firstName: 'Marry',
 //     lastName: 'Public',
@@ -34,3 +34,42 @@ mongoose.connect('mongodb://localhost:27017/play-ground',{ useNewUrlParser: true
 // }).catch(error => {
 //     console.log('Error creating student:',error.message);
 // });
+
+async function getStudents()
+{
+    let result = await Student.find();
+    console.log('displaying students',result);
+    console.log('called getStudents()');
+}
+
+// async function run()
+// {
+//     getStudents().then(function(){
+//         console.log('called run function');
+//     }).catch(error => {
+//         console.log(error.message);
+//     })
+// }
+// run().then(function () {
+//     console.log('finished running');
+
+// }).catch(error => {
+//     console.log(error.message);
+
+// });
+async function findStudentByID(id)
+{
+    await Student.findById(id).then(function (result) {
+        console.log('Student found:',result);
+
+    }).catch(error => {
+        console.log('Error finding student with ID: ',id);
+    });
+}
+
+findStudentByID('5f7ea4a65cff563fe784c33d').then(()=>{
+    console.log('finished finding student')
+
+}).catch(reason => {
+    console.log('Error with finding student')
+});
