f7447f7a683e3c6d348024acad255005ce519cc0
Navya C <navyc122@in.ibm.com>
2020-10-08 11:43:16 +0530

    mongo

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://user1:nKUJQhFHtI7UrlAM@cluster0.skdlm.mongodb.net/play-ground?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/play-ground',{ useNewUrlParser: true , useUnifiedTopology: true} )
 .then(function(){
-    console.log('connected to Mongodb Atlas');
-
-}).catch(err =>{
-    console.log('error connecting to Mongodb',err.message);
+    console.log('Connected to Mongo');
+})
+.catch(reason => {
+    console.log('Connection is not Unsuccessful..!',reason.message);
 })
 
+// const studentSchema=new mongoose.Schema(
+//     {
+//         firstName:String,
+//         lastName:String,
+//         email:String,
+//         doj:{type: Date,default:Date.now()}
+//     });
 
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://cluster0.okeiq.mongodb.net/<dbname>" --username user1', {useNewUrlParser: true, useUnifiedTopology: true})
-// .then(function(){
-//     console.log('connected to Mongodb Atlas');
-// }).catch(err =>{
-//     console.log('error connecting to Mongodb',err.message);
-// })
+// const Student=mongoose.model('Student',studentSchema);
+// let student=new Student({
+//     firstName: 'Marry',
+//     lastName: 'Public',
+//     email: 'marry@email.com'
+// });
+// async function createStudent()
+// {
+//     let result = await student.save();
+
+//     console.log(result);
+// }
+
+// createStudent().then(function (){
+//     console.log('Student created: ');
+
+// }).catch(error => {
+//     console.log('Error creating student:',error.message);
+// });
