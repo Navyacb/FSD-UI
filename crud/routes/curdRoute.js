ac26e0af114d6f56f63f436363a373568b0d25ef
unknown <ibmjavafse06@iiht.tech>
2020-10-12 10:31:09 +0530

    curd

--- crud/routes/curdRoute.js
+++ crud/routes/curdRoute.js
@@ -35,11 +35,13 @@ router.put('/:id', async (req, res) => {
 });
 
 
-router.delete('/:id', async (req, res) => {​​​​​​​
-  const genre = await Genre.findByIdAndRemove(req.params.id);
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  res.send(genre);
-}​​​​​​​);
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndRemove({_id: req.params.id});
+    if (!genre) {
+            return res.status(404).send('The genre with the given ID was not found.');
+        }
+    res.send(genre);
+});
 
 
 function validateGenre(genre) {
