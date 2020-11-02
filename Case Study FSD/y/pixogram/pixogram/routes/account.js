ea824eae1de5bdf0070a6f7569663b6108f9b110
Navya C <navyc122@in.ibm.com>
2020-11-02 13:05:57 +0530

    cs

---
+++ Case Study FSD/y/pixogram/pixogram/routes/account.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+/* GET search page. */
+
+router.get('/search', function(req, res, next) {
+
+    res.render('search', { data:{ titleView: 'Search Page',customer: { _id:'5f90047c447128608075a32d' },isAccount:true} });
+});
+
+router.get('/newsfeed', function(req, res, next) {
+    res.render('newsfeed', { data:{ titleView: 'NewsFeed Page',customer: { _id:'5f90047c447128608075a32d' },isAccount:true} });
+});
+
+router.get('/blocked', function(req, res, next) {
+    res.render('blockedaccounts', { data:{ titleView: 'Blocked Users Page',customer: { _id:'5f90047c447128608075a32d' },isAccount:true} });
+});
+
+router.get('/update', function(req, res, next) {
+    res.render('editprofile', { data:{ titleView: 'Update Profile Page',customer: { _id:'5f90047c447128608075a32d' },isAccount:true} });
+});
+
+module.exports = router;
