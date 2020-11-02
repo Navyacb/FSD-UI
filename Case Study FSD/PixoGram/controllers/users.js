642cd8a357500985daad8977d58591d35dd39be8
unknown <ibmjavafse06@iiht.tech>
2020-11-02 11:12:36 +0530

    case study

--- Case Study FSD/PixoGram/controllers/users.js
+++ Case Study FSD/PixoGram/controllers/users.js
@@ -2,7 +2,7 @@ const {user} = require('../models/users');
 
 exports.getUser = (id,cb) => {
     let user = user.findById({_id:id});
-    cb(false,customer);
+    cb(false,user);
 
     };
 
@@ -12,9 +12,9 @@ exports.getUsers = async (id,cb) => {
     let user=[];
     for (let i=0;i<id.length;i++) {
         console.log(id[i]);
-        let cust = await userr.findById({_id:id[i]});
+        let cust = await user.findById({_id:id[i]});
         console.log(cust);
-        customer.push(cust);
+        user.push(cust);
     }
     cb(false,user);
 };
