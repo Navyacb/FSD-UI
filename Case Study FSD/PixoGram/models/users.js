b2fcc8634501ef8ef88e0a28e0526c31d21f9bdf
Navya C <navyc122@in.ibm.com>
2020-11-02 11:18:00 +0530

    cs

--- Case Study FSD/PixoGram/models/users.js
+++ Case Study FSD/PixoGram/models/users.js
@@ -35,4 +35,13 @@ const user = mongoose.model('user', new mongoose.Schema({
 
 }));
 
+function validateuser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        phone: Joi.string().min(5).max(50).required(),
+        isGold: Joi.boolean()
+    };
+    return Joi.validate(user, schema);
+}
 exports.user = user;
+exports.validate = validateuser;
